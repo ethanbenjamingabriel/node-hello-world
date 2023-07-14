f28ab6f0bd469831c4ec702e035b3334b14ecedc
Ethan Gabriel <ethanbenjamin.gabriel@gmail.com>
2023-07-14 13:45:49 -0400

    module exports

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // commonJS: how to properly share modules from one file to another 
-require('./add');
+const addFn = require('./add');
 
 console.log("Hello World!");
+
+const sum = addFn(2, 3);
+const sum2 = addFn(3, 4);
+console.log(sum);
+console.log(sum2);
