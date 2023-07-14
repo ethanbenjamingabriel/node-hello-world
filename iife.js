ba3b4cf687514d701ba2c096eeb2f36e64a7461c
Ethan Gabriel <ethanbenjamin.gabriel@gmail.com>
2023-07-14 14:03:28 -0400

    module wrapper

--- iife.js
+++ iife.js
@@ -1,11 +1,11 @@
 // Immediately Invoked Function Expression (IIFE) - before modules code is executed , node will wrap it with function wrapper that provides module scope
 // avoids conflicts in variables and functions
-(function(){
+(function(message){
     const superHero = "Batman";
-    console.log(superHero);
-})();
+    console.log(message, superHero);
+})('Hello'); // module wrapper
 
-(function(){
+(function(message){
     const superHero = "Superman";
-    console.log(superHero);
-})();
+    console.log(message, superHero);
+})('Hey');
